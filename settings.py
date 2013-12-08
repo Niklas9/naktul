1d8177e3bd4de56f8a2de98401a21b985afe25fb
Niklas Andersson <nandersson900@gmail.com>
2013-12-08 09:07:24 -0800

    switched so that bzip2 is the standard algorithm used

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ BACKUP_DIRS = ('/var/www',)
 BACKUP_DAYS_TO_STORE = 7  # number of days
 BACKUP_SAVE_EVERY_MONTH = True  # stores the backup from the first every month
 BACKUP_LOG_FILE = 'nback.log'
-BACKUP_COMPRESSION_ALGO = 'gz'  # viable options: 'bz2', 'gz'
+BACKUP_COMPRESSION_ALGO = 'bz2'  # viable options: 'bz2', 'gz'
 
 # MySQL database
 USE_MYSQL = True
