5e3cbdd253bbb135675edd8c1a8622dc7211235b
Niklas Andersson <nandersson900@gmail.com>
2014-01-08 05:02:08 -0800

    set smtp username and password default to none, and using smtp tls to false as default as well

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ BACKUP_DIRS = ('/var/www',)
 BACKUP_DAYS_TO_STORE = 7  # number of days
 BACKUP_SAVE_EVERY_MONTH = True  # stores the backup from the first every month
 BACKUP_LOG_FILE = 'nback.log'
-BACKUP_COMPRESSION_ALGO = 'bz2'  # viable options: 'bz2', 'gz'
+BACKUP_COMPRESSION_ALGO = 'gz'  # viable options: 'bz2', 'gz'
 
 # MySQL database
 USE_MYSQL = True
@@ -20,11 +20,11 @@ BACKUP_MONGODB_DBS = ('test',)
 # Notification settings
 EMAIL_FROM = 'info@example.com'
 EMAIL_CONTACTS = ('your@email.com',)
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_TLS_PORT = 587
 SMTP_HOST = 'localhost'
-SMTP_USERNAME = ''
-SMTP_PASSWD = ''
+SMTP_USERNAME = None
+SMTP_PASSWD = None
 
 # Amazon AWS S3 settings
 AWS_BUCKET = 'servers'
